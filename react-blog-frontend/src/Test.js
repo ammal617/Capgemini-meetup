4c45b78c259eb0f54827bc7545a92f1d5d0d8e06
Ogren <mikael.j.ogren@capgemini.com>
2018-04-05 20:37:15 +0200

    Removed junk files

--- react-blog-frontend/src/Test.js
+++
@@ -1,8 +0,0 @@
-import AWS from 'aws-sdk';
-
-AWS.config.update({region: 'eu-west-1', credentials: {
-  aws_access_key_id = 'AKIAIVPSO7WV2REW3VLA',
-  aws_secret_access_key = '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'
-}});
-var lambda = new AWS.Lambda({ region: "eu-west-1", apiVersion: "2015-03-31" });
-
